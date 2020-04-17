00fb9f509f04649aae08008fd85ff53f190e6808
@slovacus <slovacus@gmail.com>
2020-04-17 04:20:19 -0500

    chore: implement restic password and restic file (#10)

--- zsh-restic.zsh
+++ zsh-restic.zsh
@@ -13,27 +13,14 @@ RESTIC_SRC_DIR="${RESTIC_ROOT_DIR}"/src
 export GHQ_MESSAGE_BREW="Please install brew or use antibody bundle luismayta/zsh-brew branch:develop"
 
 [ -z "${RESTIC_FILE_EXCLUDE}" ] && export RESTIC_FILE_EXCLUDE="${HOME}/tmp/restic_excludes"
+[ -n "${RESTIC_PASSWORD}" ] && echo "${RESTIC_PASSWORD}" > "${RESTIC_PASSWORD_FILE}"
 [ -z "${RESTIC_REPOSITORY}" ] && export RESTIC_REPOSITORY=/Volumes/MrRobot/Backup
-
-cat > "${RESTIC_FILE_EXCLUDE}" << EOF
-$HOME/.cache
-$HOME/.dbus
-$HOME/.gvfs
-$HOME/.kube/cache
-$HOME/.kube/http-cache
-$HOME/.config
-$HOME/.cookiecutter_replay
-$HOME/.cookiecutters
-$HOME/gdrive
-$HOME/go/pkg/mod
-$HOME/go/src
-$HOME/snap
-$HOME/Projects
-EOF
 
 # shellcheck source=/dev/null
 source "${RESTIC_SRC_DIR}"/base.zsh
 
-function backup {
-    restic backup -r "${RESTIC_REPOSITORY}" --exclude-file "${RESTIC_FILE_EXCLUDE}" "${HOME}"
+function restic::backup {
+    restic backup -r "${RESTIC_REPOSITORY}" "${HOME}/.ssh" -v 2
+    restic backup -r "${RESTIC_REPOSITORY}" "${HOME}/Documents" -v 2
+    restic backup -r "${RESTIC_REPOSITORY}" /Volumes/Keybase/team/* -v 2
 }
