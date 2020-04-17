3c6db27c426a5c6a92a76b21cece19b52c74056e
@slovacus <slovacus@gmail.com>
2020-04-17 00:50:45 -0500

    chore: implement base source (#10)

--- zsh-restic.zsh
+++ zsh-restic.zsh
@@ -7,15 +7,10 @@
 # Authors:
 #   Luis Mayta <slovacus@gmail.com>
 #
-restic_package_name=restic
+export RESTIC_PACKAGE_NAME="restic"
+RESTIC_ROOT_DIR=$(dirname "$0")
+RESTIC_SRC_DIR="${RESTIC_ROOT_DIR}"/src
+export GHQ_MESSAGE_BREW="Please install brew or use antibody bundle luismayta/zsh-brew branch:develop"
 
-function restic::install {
-    message_info "Installing ${restic_package_name}"
-    if ! type -p brew > /dev/null; then
-        message_error "it's neccesary brew, add: luismayta/zsh-brew"
-    fi
-    brew install restic
-    message_success "Installed ${restic_package_name}"
-}
-
-if ! type -p restic > /dev/null; then restic::install; fi
+# shellcheck source=/dev/null
+source "${RESTIC_SRC_DIR}"/base.zsh
