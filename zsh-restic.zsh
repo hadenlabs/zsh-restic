efc4fcb93ab12ace21e6fe4fe8eabec31183e3c2
@slovacus <slovacus@gmail.com>
2020-04-18 09:18:26 -0500

    chore: clean var for password restic command (#12)

--- zsh-restic.zsh
+++ zsh-restic.zsh
@@ -13,6 +13,7 @@ RESTIC_SRC_DIR="${RESTIC_ROOT_DIR}"/src
 export GHQ_MESSAGE_BREW="Please install brew or use antibody bundle luismayta/zsh-brew branch:develop"
 
 [ -z "${RESTIC_FILE_EXCLUDE}" ] && export RESTIC_FILE_EXCLUDE="${HOME}/tmp/restic_excludes"
+[ -z "${RESTIC_PASSWORD_COMMAND}" ] && unset RESTIC_PASSWORD_COMMAND
 [ -n "${RESTIC_PASSWORD}" ] && echo "${RESTIC_PASSWORD}" > "${RESTIC_PASSWORD_FILE}"
 [ -z "${RESTIC_REPOSITORY}" ] && export RESTIC_REPOSITORY=/Volumes/MrRobot/Backup
 
