40467feb6f2a6a14b765df4b232e8327832305b8
@slovacus <slovacus@gmail.com>
2020-04-19 01:49:16 -0500

    chore: implement files and path exlude (#12)

--- src/base.zsh
+++ src/base.zsh
@@ -3,6 +3,53 @@
 
 cat > "${RESTIC_FILE_EXCLUDE}" << EOF
     ._*
+.DS_Store
+*.pyc
+*.orig
+*.retry
+*.tmp
+*.egg
+.eggs
+*.egg-inf
+.pytest_cache
+.hypothesis
+.mypy_cache
+.hypothesis
+.lib-cov
+__pycache__
+env.back
+venv.back
+coverage
+node_modules
+jspm_packages
+typings
+.npm
+.vagrant
+.wercker
+.eggs
+eggs
+*.egg-info
+.pytest_cache
+.hypothesis
+docs/_build/
+htmlcov
+.mypy_cache
+.hypothesis
+wheels
+.lib-cov
+bower_components
+.venv
+env.back
+venv.back
+.terraform
+.next
+.nuxt
+.cache
+.grunt
+.vuepress/dist
+.fusebox
+.dynamodb
+
 EOF
 
 function restic::install {
