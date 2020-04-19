3ae397326a2d4208991adf098abe75986fe2005b
@slovacus <slovacus@gmail.com>
2020-04-19 01:31:33 -0500

    chore: implement functions for restic path(#12)

--- zsh-restic.zsh
+++ zsh-restic.zsh
@@ -12,17 +12,32 @@ RESTIC_ROOT_DIR=$(dirname "$0")
 RESTIC_SRC_DIR="${RESTIC_ROOT_DIR}"/src
 export GHQ_MESSAGE_BREW="Please install brew or use antibody bundle luismayta/zsh-brew branch:develop"
 
-[ -z "${RESTIC_FILE_EXCLUDE}" ] && export RESTIC_FILE_EXCLUDE="${HOME}/tmp/restic_excludes"
 [ -z "${RESTIC_PASSWORD_COMMAND}" ] && unset RESTIC_PASSWORD_COMMAND
-[ -n "${RESTIC_PASSWORD}" ] && echo "${RESTIC_PASSWORD}" > "${RESTIC_PASSWORD_FILE}"
+[ -z "${RESTIC_FILE_EXCLUDE}" ] && export RESTIC_FILE_EXCLUDE="${HOME}/tmp/restic_excludes"
 [ -z "${RESTIC_REPOSITORY}" ] && export RESTIC_REPOSITORY=/Volumes/MrRobot/Backup
+[ -z "${RESTIC_PATHS_BACKUP}" ] && export RESTIC_PATHS_BACKUP=(
+        "${HOME}/.ssh"
+        "${HOME}/Documents"
+        /Volumes/Keybase/private/luismayta/*
+        /Volumes/Keybase/team/*
+    )
+
+
+[ -n "${RESTIC_PASSWORD}" ] && echo "${RESTIC_PASSWORD}" > "${RESTIC_PASSWORD_FILE}"
 
 # shellcheck source=/dev/null
 source "${RESTIC_SRC_DIR}"/base.zsh
 
+# restic::backup function for backup
 function restic::backup {
-    restic backup -r  "${RESTIC_REPOSITORY}" "${HOME}/.ssh" -v 2 --exclude-file "${RESTIC_FILE_EXCLUDE}"
-    restic backup -r "${RESTIC_REPOSITORY}" "${HOME}/Documents" -v 2 --exclude-file "${RESTIC_FILE_EXCLUDE}"
-    restic backup -r "${RESTIC_REPOSITORY}" /Volumes/Keybase/private/luismayta/* -v 2 --exclude-file "${RESTIC_FILE_EXCLUDE}"
-    restic backup -r "${RESTIC_REPOSITORY}" /Volumes/Keybase/team/* -v 2 --exclude-file "${RESTIC_FILE_EXCLUDE}"
+    local path_name
+    path_name="${1}"
+    restic backup -r "${RESTIC_REPOSITORY}" "${path_name}" -v 2 --exclude-file "${RESTIC_FILE_EXCLUDE}"
+}
+
+# restic::backup::all backup all paths of var
+function restic::backup::all {
+    for path_name in "${RESTIC_PATHS_BACKUP[@]}"; do
+        restic::backup "${path_name}"
+    done
 }
